eb5c8e0b6d9369057803c6590d1fbfeb866dd643
github username <github email address>
2019-05-13 13:38:59 -0700

    changed repository name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.create(user_params)
+        @user = User.find_or_create_by(user_params)
         render json: @user
     end
 
